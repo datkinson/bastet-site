a2a77b4d67ec8a216c216d8eb4f42e55d69627fa
Daniel Atkinson <hourd.tasa@gmail.com>
2015-05-03 16:41:59 +0100

    added site template

---
+++ src/users/UserService.js
@@ -0,0 +1,46 @@
+(function(){
+  'use strict';
+
+  angular.module('users')
+         .service('userService', ['$q', UserService]);
+
+  /**
+   * Users DataService
+   * Uses embedded, hard-coded data model; acts asynchronously to simulate
+   * remote data service call(s).
+   *
+   * @returns {{loadAll: Function}}
+   * @constructor
+   */
+  function UserService($q){
+    var users = [
+      {
+        name: 'Server',
+        avatar: 'svg-1',
+        icon: '/img/icon-black.svg',
+        status: 'Coming Soon',
+        content: 'Proxy Application to link between all different kinds of home automation / internet of things devices.',
+        git: 'https://github.com/martindevans/bastet',
+        projectName: 'Bastet'
+      },
+      {
+        name: 'Client',
+        avatar: 'svg-2',
+        icon: '/img/icon-white.svg',
+        status: 'Coming Soon',
+        content: 'A client application to manage and interact with baset via its open API',
+        git: 'https://github.com/datkinson/bastet-node',
+        projectName: 'Bastet-Node'
+      }
+    ];
+
+    // Promise-based API
+    return {
+      loadAllUsers : function() {
+        // Simulate async nature of real remote calls
+        return $q.when(users);
+      }
+    };
+  }
+
+})();
